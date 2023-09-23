452f524cdccc2997a25a7e8d153e59a7dd7ba4eb
brianjhudson <brianjasonhudson@gmail.com>
2023-09-23 13:51:50 -0500

    Add functionality and validation

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
+const OpenAI = require("openai");
+const {object, string, array, validate} = require('yup');
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
 
 const app = express();
 app.use(bodyParser.json());
 
-const instance = axios.create({
-    baseURL: 'https://api.openai.com/v1/chat/completions',
-    timeout: 1000,
-    headers: {'Authorization': `Bearer ${process.env.CHAT_KEY}`}
-});
 
 app.get("/", (req, res, next) => {
     return res.status(200).json("Hello world");
 });
 
 app.post("/send-message", async (req, res, next) => {
-    const chatGPT = await axios.post(res.body);
-    console.log("This is where we send a message to Chat GPT");
-    return res.status(200).json({
-        message: "Response",
-        history: ["1", "2"]
-    });
+    let body;
+    try {
+        const bodySchema = object({
+            model: string().required(),
+            messages: array().of(object({
+                role: string().required(),
+                content: string().required()
+            }))
+        })
+        body = await bodySchema.validate(req.body);
+    }
+    catch (e) {
+        return res.status(400).json('Invalid input');
+    }
+    try {
+        const chatCompletion = await openai.chat.completions.create(body);
+      
+        if (!chatCompletion || !chatCompletion.choices || !chatCompletion.choices[0] || !chatCompletion.choices[0].message) {
+            return res.status(500).json({
+                message: 'No response received from server'
+            });
+        }
+        return res.status(200).json(chatCompletion.choices[0].message);
+    } catch (e) {
+        return res.status(500).json(e.message || 'Error processing response from server');
+    }
 });
 
 app.listen(process.env.PORT || 3000, undefined, () => {
