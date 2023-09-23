bd2d51421abe1e9d65bb06c42b2054676b3c2095
brianjhudson <brianjasonhudson@gmail.com>
2023-09-23 12:18:17 -0500

    Add license and modules

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const instance = axios.create({
+    baseURL: 'https://api.openai.com/v1/chat/completions',
+    timeout: 1000,
+    headers: {'Authorization': `Bearer ${process.env.CHAT_KEY}`}
+});
 
 app.get("/", (req, res, next) => {
     return res.status(200).json("Hello world");
 });
 
-app.post("/send-message", (req, res, next) => {
+app.post("/send-message", async (req, res, next) => {
+    const chatGPT = await axios.post(res.body);
     console.log("This is where we send a message to Chat GPT");
     return res.status(200).json({
         message: "Response",
